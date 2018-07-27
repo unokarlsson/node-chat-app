98bf430de49029d2f8524caacf75b83ad95d6c86
Uno Karlsson <uno.carlsson@gmail.com>
2018-07-27 17:16:20 +0200

    Added connect and disonnet functions

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const path = require('path');
+const http = require('http');
+
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 // console.log('Old way          = ',__dirname + '/../public');
@@ -7,13 +10,26 @@ console.log('Using path.join  = ',publicPath);
 
 const port = process.env.PORT || 3000;
 
-const app = express();
+let app     = express();
+let server  = http.createServer(app);
+
+// Setup socket.io on the server
+// A socket.io client side lib is available at:
+// http://localhost:3000/socket.io/socket.io.js
+let io = socketIO(server);
+
+io.on('connection',(socket) => {
+    console.log('New user connected!');
+
+    socket.on('disconnect',() =>{
+        console.log('User was disconnected!');
+    });
+});
+
+
 app.use(express.static(publicPath));
 
-// app.get('/',(request,response) => {
-//     response.send(publicPath/index.html);
-// });
 
-app.listen(port,() => {
+server.listen(port,() => {
     console.log(`Server up on port ${port}`);
 })
