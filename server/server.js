374f4bd4f88cac15aafde935ed06f69b7657486f
Uno Karlsson <uno.carlsson@gmail.com>
2018-07-27 14:45:29 +0200

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname,'../public');
+// console.log('Old way          = ',__dirname + '/../public');
+console.log('Using path.join  = ',publicPath);
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.static(publicPath));
+
+// app.get('/',(request,response) => {
+//     response.send(publicPath/index.html);
+// });
+
+app.listen(port,() => {
+    console.log(`Server up on port ${PORT}`);
+})
