5ac58f63e3b19e908ec740213749d9b1e2dfc224
Uno Karlsson <uno.carlsson@gmail.com>
2018-07-29 22:11:03 +0200

    Add from for messages and show incoming messages.

--- public/js/index.js
+++ public/js/index.js
@@ -2,19 +2,32 @@
 let socket = io();
 socket.on('connect', function() {
     console.log('Connected to server!');
-
-    // socket.emit('createMessage',{
-    //     frome: 'alexandra@karlsson.com',
-    //     text: 'Hey, This is Uno.'
-    // })
 });
-
-socket.on('newMessage',function(message) {
-    console.log('New message',message);
-});
-
 
 socket.on('disconnect',function() {
     console.log('Disconnected from server!');
 });
 
+socket.on('newMessage',function(message) {
+    console.log('New message',message);
+    const listItem = jQuery('<li></li>');
+    listItem.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(listItem);
+});
+
+// socket.emit('createMessage',{   
+//     from: 'Frank',
+//     text: 'Hi from Frank'
+// }, function(message) {
+//     console.log('Got it:',message);
+// });
+
+jQuery('#message-form').on('submit',function (event) {
+    event.preventDefault();
+    socket.emit('createMessage',{
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    },function (messge) {
+
+    });
+});
